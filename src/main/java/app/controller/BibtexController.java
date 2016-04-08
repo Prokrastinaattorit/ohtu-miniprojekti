607eb1c44195ee97285df6f860bd66e16b0710ff
Laur Pulliainen <pulliainen.laur@gmail.com>
2016-04-08 17:12:18 +0300

    lisaaminen

--- src/main/java/app/controller/BibtexController.java
+++ src/main/java/app/controller/BibtexController.java
@@ -5,7 +5,13 @@
  */
 package app.controller;
 
+import app.domain.Book;
+import app.repositories.BookRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -16,9 +22,30 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @Controller
 @RequestMapping("/bibtexinator")
 public class BibtexController {
+    
+    @Autowired
+    BookRepository bookRepository;
+    
+    @ModelAttribute
+    private Book getBook() {
+        return new Book();
+    }
 
     @RequestMapping(method = RequestMethod.GET)
-    public String redirect() {
+    public String redirect(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
         return "bibtexinator";
+    }
+    
+    @RequestMapping(method = RequestMethod.POST, value="/save")
+    public String saveBook(@ModelAttribute Book book,
+            BindingResult bindingResult) {
+        
+        if (bindingResult.hasErrors()) {
+            return "bibtexinator";
+        }
+        bookRepository.save(book);
+        
+        return "redirect:/bibtexinator";
     }
 }
