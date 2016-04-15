97305d68456a8ad497123042a0cf83134fde5435
Laur Pulliainen <pulliainen.laur@gmail.com>
2016-04-15 15:56:30 +0300

    article luokka ja repo

---
+++ src/main/java/app/domain/Article.java
@@ -0,0 +1,65 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.domain;
+
+import javax.persistence.Entity;
+import org.springframework.data.jpa.domain.AbstractPersistable;
+
+/**
+ *
+ * @author Sara ja Laur
+ */
+@Entity
+public class Article extends AbstractPersistable<Long> implements Entry {
+    
+    private String author;
+    private String title;
+    private String journal;
+    private String volume;
+    private String pages;
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getJournal() {
+        return journal;
+    }
+
+    public void setJournal(String journal) {
+        this.journal = journal;
+    }
+
+    public String getVolume() {
+        return volume;
+    }
+
+    public void setVolume(String volume) {
+        this.volume = volume;
+    }
+
+    public String getPages() {
+        return pages;
+    }
+
+    public void setPages(String pages) {
+        this.pages = pages;
+    }
+
+    
+}
---
+++ src/main/java/app/repositories/ArticleRepository.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.repositories;
+
+import app.domain.Article;
+import java.io.Serializable;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author Sara ja Laur
+ */
+public interface ArticleRepository extends JpaRepository<Article, Long>{
+    
+}
