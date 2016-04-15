a68d9c2e142432ee9c87f425793bf1e79733aaa0
Milla Peltoniemi <tunturikissa@gmail.com>
2016-04-15 16:33:03 +0300

    Created BibTexGenerator.class

---
+++ src/test/java/app/filegeneration/BibTexGeneratorTest.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package app.filegeneration;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author milla
+ */
+public class BibTexGeneratorTest {
+    public BibTexGenerator bibtexnator;
+    
+    public BibTexGeneratorTest() {
+    }
+    
+    
+    @Before
+    public void setUp() {
+        bibtexnator = new BibTexGenerator();
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    // TODO add test methods here.
+    // The methods must be annotated with annotation @Test. For example:
+    //
+    // @Test
+    // public void hello() {}
+}
